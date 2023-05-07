31b8f44b06e537b15257709a2ae8940ebafad626
bab88200 <chanitebrandon@gmail.com>
2023-05-07 16:39:57 +0200

    maj navbar

--- public/js/nav.js
+++ public/js/nav.js
@@ -31,21 +31,5 @@ document.addEventListener("wheel", (e) => {
     bulle.style.transition = "0.15s"
 });
 
-// La fermeture de mon menu mobile version responsive
-const menuMobile = document.querySelector('.menu-mb');
-const navLinks = document.querySelector('.nav-links');
 
-menuMobile.addEventListener('click', ()=>{
-    navLinks.classList.toggle('mobile-menu')
-})
 
-let typed = new Typed('.auto-typing', {
-    strings: ['LYCÉE VIVIANI'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    loop: true,
-    fadeOut: true,
-    fadeOutClass: 'typed-fade-out',
-    fadeOutDelay: 500,
-    shuffle: true,
-});
